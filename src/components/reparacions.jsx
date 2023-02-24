96cb251b1ef3f87e8e831acff364225326b60772
Nando <nandet2003@gmail.com>
2023-02-24 12:55:39 +0100

    obtindre reparacions

--- src/components/reparacions.jsx
+++ src/components/reparacions.jsx
@@ -7,78 +7,98 @@ const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/))
 
 export default function Reparacions() {
     const [reparaciones, setReparaciones] = useState([]);
-    const [user, setUser] = useState([]);
+    const [fecha, setFecha] = useState([]);
+    const [estat, setEstat] = useState([]);
     const [token, setToken] = useState(localStorage.getItem('token'));
     const [id, setId] = useState(localStorage.getItem('userId'));
     const navigate = useNavigate()
 
-    const obtenerUsuario  = () => {
-        if(token == null){
+    const obtenerUsuario = async function () {
+        if (token == null) {
             navigate('/login');
         }
-        fetch('http://api/api/users/'+id, {
+        const response = await fetch('http://api/api/users/' + id, {
             method: 'GET',
             headers: {
-                'Authorization': 'Bearer '+token,
-              'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
             },
-          })
-            .then(response => response.json())
-            .then(data => {
-                setUser(data);
-                obtenerReparaciones();
-            })
+        })
+        const data = await response.json();
+        obtenerReparaciones(data);
+
     }
+
     useEffect(() => {
-        obtenerUsuario();
+        obtenerUsuario()
     }, [])
 
-    const obtenerReparaciones = () => {
-        if (user.role == 'ROLE_USER'){
-            console.log(user.reparations);
+    const obtenerReparaciones = (user) => {
+        if (user.role === 'ROLE_USER') {
+            setReparaciones([...user.reparations]);
         }
-        else if (user.role == 'ROLE_WORKER'){
-            console.log(user.repairs);
+        else if (user.role === 'ROLE_WORKER') {
+            setReparaciones([...user.repairs]);
         }
+        prepararAtributos();
     }
 
-    return (
-        <main>
+    const prepararAtributos = () => {
+        reparaciones.map((item, ind) => {
+            if (item.status === 'diagnostic') {
+                setEstat([...'EN DIAGNOSTIC']);
+            } else if (item.status === 'entregat') {
+                setEstat([...'ENTREGAT']);
+            } else if (item.status === 'llest') {
+                setEstat([...'LLEST PER A ARREPLEGAR']);
+            } else if (item.status === 'reparacio') {
+                setEstat([...'EN REPARACIO']);
+            }
+            setFecha([...new Date(item.date)])
+        })
+    }
+
+
+return (
+    <main>
         <div className="container">
             <div className="row">
                 <div className="col-12">
                     <h2>REPARACIONS </h2>
-                </div>    
+                </div>
             </div>
         </div>
         <div className="container">
             <div className="row">
                 <div className="col-12 cercador">
                     <input type="text" placeholder=" INTRODUEIX NOM O NUMERO DE REPARACIÓ" />
-                </div>    
-            </div>
-        </div>
-        <div className="container">
-            <div className="row">
-                <div className="col-6 titol-rep">
-                    <h3>REPARACIO 1</h3>
-                </div>
-                <div className="col-6">
-                </div>
-            </div>
-            <div className="row fila">
-                <div className="col-6 descripcio">
-                    <p>El dispositiu presenta colps en la pantalla,
-                        hi ha que procedir a cambiar-la. 
-                    </p>
-                </div>
-                <div className="col-6 detalls">
-                    <p><label>PRODUCTE :</label> PORTATIL LENOVO</p>
-                    <p className="reparacio"><label>ESTAT :</label> EN REPARACIÓ</p>
-                    <p><label>FECHA PREVISTA :</label> 26/01/2023</p>
                 </div>
             </div>
         </div>
+        {reparaciones.map((item, ind) => {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 titol-rep">
+                            <h3>REPARACIO {ind + 1}</h3>
+                        </div>
+                        <div className="col-6">
+                        </div>
+                    </div>
+                    <div className="row fila">
+                        <div className="col-6 descripcio">
+                            <p>{item.description}
+                            </p>
+                        </div>
+                        <div className="col-6 detalls">
+                            <p><label>PRODUCTE:</label> {item.name}</p>
+                            <p className="reparacio"><label>ESTAT: </label>{estat[ind]}</p>
+                            <p><label>FECHA PREVISTA:</label> {fecha[ind]}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })}
     </main>
-    )
+)
 }
