7e62e430dbdb345af8d55ccb6e65b25f137c3b86
Nando <nandet2003@gmail.com>
2023-02-27 10:46:18 +0100

    afegir botons de eliminar editar + reparacions adm

--- src/components/reparacions.jsx
+++ src/components/reparacions.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import '../css/reparacions.css';
 import { importAll } from './images';
+import { Button, Modal } from 'react-bootstrap'
 import { useNavigate } from "react-router-dom";
 const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
 
 
 export default function Reparacions() {
+    let boto = '';
     const [reparaciones, setReparaciones] = useState([]);
-    const [fecha, setFecha] = useState([]);
-    const [estat, setEstat] = useState([]);
+    const [rol, setRol] = useState('');
     const [token, setToken] = useState(localStorage.getItem('token'));
     const [id, setId] = useState(localStorage.getItem('userId'));
     const navigate = useNavigate()
@@ -29,17 +30,70 @@ export default function Reparacions() {
 
     }
 
+    const totesReparacions = async function () {
+        if (token == null) {
+            navigate('/login');
+        }
+        const response = await fetch('http://api/api/reparations', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json();
+        setReparaciones(data['hydra:member'])
+
+    }
+
     useEffect(() => {
         obtenerUsuario()
     }, [])
 
+    const inicializeModal=(type,item)=>{
+      /*  setTypeModal(type);
+        if(type==='insertar'){
+            formiknando.values.nombre='';
+            formiknando.values.año_nacimiento=0;
+        }else{
+            formiknando.values._id= item._id
+            formiknando.values.nombre=item.nombre;
+            formiknando.values.año_nacimiento=item.año_nacimiento;
+        }
+        handleShow();*/
+    }
+
     const obtenerReparaciones = (user) => {
         if (user.role === 'ROLE_USER') {
             setReparaciones([...user.reparations]);
+            setRol('user')
         }
         else if (user.role === 'ROLE_WORKER') {
             setReparaciones([...user.repairs]);
+            setRol('worker')
         }
+        else{
+            setRol('admin')
+            totesReparacions();
+        }
+    }
+    const handleDelete =(id,ind) =>{
+        /*fetch('https://serverred.es/api/autores/'+id, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              }
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                autores.splice(ind,1);
+                setAutores([...autores])
+                console.log(data);
+              })
+              .catch(error=>{
+                console.error('Error:', error);
+              })*/
+
     }
 
 
@@ -51,6 +105,7 @@ return (
                     <h2>REPARACIONS</h2>
                 </div>
             </div>
+            {rol==='admin'?<Button onClick={()=> inicializeModal("insertar")} className='btn btn-info'>NOVA REPARACIÓ</Button>:""}
         </div>
         <div className="container">
         </div>
@@ -73,6 +128,9 @@ return (
                             <p><label>PRODUCTE:</label> {item.name}</p>
                             <p className="reparacio"><label>ESTAT: </label> {item.status}</p>
                             <p><label>FECHA PREVISTA:</label> {item.date}</p>
+                            {rol==='admin'?<p>Usuari: {item.owner.username}</p>:""}
+                            {rol==='worker'||rol==='admin'?<Button onClick={()=> inicializeModal("editar",item)} className='btn btn-primary'>EDITAR</Button>:""}
+                            {rol==='admin'?<Button onClick={()=> handleDelete(item.id,ind)} className='btn btn-danger'>ELIMINAR</Button>:""}
                         </div>
                     </div>
                 </div>
